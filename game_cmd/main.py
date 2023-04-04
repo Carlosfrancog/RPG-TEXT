c8a9a7fef6d599f607422a826d8a78a5304f8ff1
Carlosfrancog <carlosfranco.contato@gmail.com>
2023-04-04 12:55:56 -0300

    first commit

---
+++ game_cmd/main.py
@@ -0,0 +1,202 @@
+import random
+from time import sleep
+import os
+
+os.system('cls' or 'clear')
+
+
+
+print("""
+                                            -------------------------------------------------------------------------------------                
+                                            |  ##   ##   ##  ##             ####   ### ###   #####    ######    ####    ######  |
+                                            |  ### ###   ##  ##            ##  ##  ##   ##  ##   ##   # ## #   ##  ##   ##      |
+                                            |  #######    #  #            ##       ##   ##  ##   ##     ##    ##        ##      |
+                                            |  #######    ####            ##       #######  ##   ##     ##    ##        #####   |
+                                            |  ## # ##     ##             ##       ##   ##  ##   ##     ##    ##        ##      |
+                                            |  ##   ##     ##              ##  ##  ##   ##  ##   ##   # ## #   ##  ##   ##      |
+                                            |  ##   ##     ##               ####   ### ###   #####    ######    ####    ######  |
+                                            -------------------------------------------------------------------------------------
+
+            """)
+menu_quest = str(input(''' 
+         [A] para jogar
+         [B] para ir para as confingurações
+         [C] para sair
+         :''')).upper().strip()[0]
+
+while menu_quest not in 'ABC':
+   print("""
+                                            -------------------------------------------------------------------------------------                
+                                            |  ##   ##   ##  ##             ####   ### ###   #####    ######    ####    ######  |
+                                            |  ### ###   ##  ##            ##  ##  ##   ##  ##   ##   # ## #   ##  ##   ##      |
+                                            |  #######    #  #            ##       ##   ##  ##   ##     ##    ##        ##      |
+                                            |  #######    ####            ##       #######  ##   ##     ##    ##        #####   |
+                                            |  ## # ##     ##             ##       ##   ##  ##   ##     ##    ##        ##      |
+                                            |  ##   ##     ##              ##  ##  ##   ##  ##   ##   # ## #   ##  ##   ##      |
+                                            |  ##   ##     ##               ####   ### ###   #####    ######    ####    ######  |
+                                            -------------------------------------------------------------------------------------
+
+            """)
+   print('OPÇÃO INVÁLIDA!') 
+   menu_quest = str(input(''' 
+         [A] para jogar
+         [B] para ir para as confingurações
+         [C] para sair
+         :''')).upper().strip()[0]
+
+
+if menu_quest in 'A':
+   sleep(0.5)
+   os.system('cls' or 'clear')
+   print('''
+            Olá forasteiro, vejo que não é desse mundo! 
+            Me diga então seu nome para que eu possa me lembrar de você
+                             _       _
+                            (_\     /_)
+                              ))   ((
+                            .-"""""""-.
+                        /^\/  _.   _.  \/^\ 
+                        \(   /__\ /__\   )/
+                         \,  \_°/_\°_/  ,/
+                           \    (_)    /
+                            `-.'==='.-'
+                             __) - (__
+                            /  `~~~`  \ 
+                           /  /     \  \ 
+                           \ :       ; /
+                            \|==(*)==|/
+                             :       :
+                              \  |  /
+                            ___)=|=(___
+                           (____/ \____)''')
+      
+   name = str(input('DIGITE AQUI O SEU NOME: '))
+   os.system('cls' or 'clear')
+   print(f'Muito bem, {name}')
+   print(f'''
+            Alguma vez você já se perguntou se controla realmente suas ações
+            ou se apenas tem a ilusão de que está controlando?
+            Saiba que você {name} está entrando em campos muito perigosos...
+            Neste mundo ninguém é confiavél. Será que são eles que estão seguindo
+            as suas vidas como querem ou tem algo além por trás? Se ficou curioso
+            te convido a tentar descobir, e te proponho um desáfio. Me diga o nome
+            de três seres divinos que estão escondidos nesse mundo, se conseguir, eu te 
+            revelo se isso é apenas um jogo ou não...
+                             _       _
+                            (_\     /_) 
+                              ))   ((
+                            .-"""""""-.
+                        /^\/  _.   _.  \/^\ 
+                        \(   /__\ /__\   )/
+                         \,  \_°/_\°_/  ,/
+                           \    (_)    /
+                            `-.'==='.-'
+                             __) - (__
+                            /  `~~~`  \ 
+                           /  /     \  \ 
+                           \ :       ; /
+                            \|==(*)==|/
+                             :       :
+                              \  |  /
+                            ___)=|=(___
+                           (____/ \____)  
+            ''')
+   quest = str(input('Aceita? [S/N]:' )).upper().strip()[0]
+   while quest not in 'SN':
+      print('OPS! Resposta inválida!')
+      quest = str(input('Aceita? [S/N]:' )).upper().strip()[0]
+      
+   if quest in 'N':
+      print(f'''
+            Muito bem {name}, até a próxima!
+                             _       _
+                            (_\     /_)
+                              ))   ((
+                            .-"""""""-.
+                        /^\/  _.   _.  \/^\ 
+                        \(   /__\ /__\   )/
+                         \,  \_°/_\°_/  ,/
+                           \    (_)    /
+                            `-.'==='.-'
+                             __) - (__
+                            /  `~~~`  \ 
+                           /  /     \  \ 
+                           \ :       ; /
+                            \|==(*)==|/
+                             :       :
+                              \  |  /
+                            ___)=|=(___
+                           (____/ \____)''')
+      sleep(1.5)
+      os.system('cls' or 'clear')
+   else:
+      sleep(1.5)
+      os.system('cls' or 'clear')
+      print('''
+            Você aceitou o desafio de Pixie duente.
+            
+            Você abre seus olhos e percebe estar em uma pequena
+            cama de madeira, num quarto escuro, umido e fedido.
+            Consegue ver uma janela e uma porta dento do quarto, apenas.
+            ''')
+   
+      quest = str(input('''
+                     [A] Ir ver a janela
+                     [B] Ir até a porta
+                     ESCOLHA AQUI:''')).upper()[0]
+      while quest not in 'AB':
+         print('Opção inválida!')
+         quest = str(input('''
+                     [A] Ir ver a janela
+                     [B] Ir até a porta
+                     ESCOLHA AQUI:''')).upper()[0]
+      if quest in 'A': # resposta ir para a janela
+         print('''
+               Você vai para a janela, ela está trancada.
+               ao olhar pela janela você percebe estar em uma pequena vila 
+               medieaval. Mas a vila parece estar quieta, não há
+               pessoas ou animais fora das casas. O sol está se pondo e uma grande lua
+               vermelha nasce do horizonte.
+               ''')
+         quest = str(input('''
+                           [A] Dar um soco na janela
+                           [B] Ir ver a porta
+                           [C] Dar uma cabeçada na janela
+                           ESCOLHA AQUI:''')).upper()[0]
+         while quest not in 'ABC':
+            print('Opcão inválida!')
+            quest = str(input('''
+                              [A] Dar um soco na janela
+                              [B] Ir ver a porta
+                              [C] Dar uma cabeçada na janela
+                              ESCOLHA AQUI:''')).upper()[0]
+         if quest in 'A':
+            os.system('cls' or 'clear')
+            print('SOCO')
+         elif quest in 'B':
+            os.system('cls' or 'clear')
+            print('PORTA')
+         else:
+            os.system('cls' or 'clear')
+            print('CABEÇA')
+            
+      else: #resposta ir para a porta
+         print('''
+               A porta parece estar trancada, porem é muito frágil.
+               ''')
+         quest = str(input('''
+                           [A] Dar um soco na porta
+                           [B] Dar um chute na porta
+                           ESCOLHA AQUI:''')).upper()[0]
+         while quest not in 'AB':
+            print('Opção inválida!')
+            quest = str(input('''
+                              [A] Dar um soco na porta
+                              [B] Dar um chute na porta
+                              ESCOLHA AQUI:''')).upper()[0]
+         if quest in 'A':
+            os.system('cls' or 'clear')
+            print('Soco')
+         else:
+            os.system('cls' or 'clear')
+            print('chute')
